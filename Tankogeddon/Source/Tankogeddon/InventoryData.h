f5229f24040b400d5173929ce3e2bdf84b034f72
Drakonikita <mal433@ya.ru>
2022-09-11 17:51:18 +0300

    hometask 7 UI in progress

---
+++ Tankogeddon/Source/Tankogeddon/InventoryData.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+#include "Engine/DataTable.h"
+#include "Inve"
+
+UENUM()
+enum EItemType
+{
+	
+};
+
+UENUM()
+enum EItemRarity
+{
+	
+};
+
+USTRUCT(BlueprintType)
+struct FInventoryItemInfo : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category = "General")
+	FName Id;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "General")
+	FText Name;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Type")
+	EItemType Type;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Type")
+	EItemRarity Rarity;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Visual")
+	TSoftObjectPtr<UTexture2D> Icon;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Visual")
+	TSoftObjectPtr<UStaticMesh> Mesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Stats")
+	int32 Damage;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Stats")
+	int32 Armor;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Stats")
+	int32 MagicPower;
+	
+};
+
+
+USTRUCT(BlueprintType)
+struct FInventorySlotInfo : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FName Id = NAME_None;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 Count;
+	
+};
+
+
